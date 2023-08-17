37627a82b024251e9be982ddda2b493966cc3bf5
KG <KeilGriesLP@gmail.com>
2023-08-17 14:11:41 -0400

    Created product details page.

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-import 'homepage.dart';
+import 'package:shop_app/global_variables.dart';
+import 'package:shop_app/product_details_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +20,9 @@ class MyApp extends StatelessWidget {
           seedColor: const Color.fromRGBO(254, 206, 1, 1),
           primary: const Color.fromRGBO(254, 206, 1, 1),
         ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(fontSize: 20, color: Colors.black),
+        ),
         inputDecorationTheme: const InputDecorationTheme(
           hintStyle: TextStyle(
             fontWeight: FontWeight.bold,
@@ -28,6 +31,10 @@ class MyApp extends StatelessWidget {
           prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
         ),
         textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 35,
+          ),
           titleMedium: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 20,
@@ -37,8 +44,11 @@ class MyApp extends StatelessWidget {
             fontSize: 16,
           ),
         ),
+        useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: ProductDetailsPage(
+        product: products[0],
+      ),
     );
   }
 }
